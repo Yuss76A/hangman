a2d55df6af28abedac47bbfc491e76023fab439d
Yusein Ali <yuse_91yus@hotmail.com>
2024-10-13 08:29:18 +0000

    Refactored word_list for better readibility

--- words.py
+++ words.py
@@ -1,12 +1,13 @@
 word_list = [
-'agent', 'code', 'spy', 'secret', 'mission', 'covert', 'gadget',
-'briefcase', 'disguise', 'wire', 'bug', 'cipher', 'dossier', 'shadow',
-'intel', 'mole', 'cover', 'password', 'alias', 'undercover', 'stealth',
-'decoy', 'contact', 'safe', 'rendezvous', 'cipher', 'wiretap', 'handler',
-'escape', 'operative', 'mole', 'protocol', 'stakeout', 'vest', 'classified',  # noqa
-'infiltrate', 'sting', 'tactical', 'secure', 'hidden', 'pursuit', 'tracking',  # noqa
-'asset', 'sabotage', 'recon', 'covert', 'leak', 'sneak', 'breach', 'double',  # noqa
-'defect', 'diplomat', 'extraction', 'fugitive', 'sleeper', 'enigma',
-'codebook', 'surveil', 'target', 'deniable', 'bait', 'defuse', 'counter',
-'trace', 'chatter', 'clandestine', 'dispatch', 'evasive', 'intercept', 'plant'  # noqa
+    'agent', 'code', 'spy', 'secret', 'mission', 'covert', 'gadget',
+    'briefcase', 'disguise', 'wire', 'bug', 'cipher', 'dossier', 'shadow',
+    'intel', 'mole', 'cover', 'password', 'alias', 'undercover', 'stealth',
+    'decoy', 'contact', 'safe', 'rendezvous', 'cipher', 'wiretap', 'handler',
+    'escape', 'operative', 'mole', 'protocol', 'stakeout', 'vest',
+    'infiltrate', 'sting', 'tactical', 'secure', 'hidden', 'pursuit',
+    'asset', 'sabotage', 'recon', 'covert', 'leak', 'sneak', 'breach',
+    'defect', 'diplomat', 'extraction', 'fugitive', 'sleeper', 'enigma',
+    'codebook', 'surveil', 'target', 'deniable', 'bait', 'defuse', 'counter',
+    'trace', 'chatter', 'clandestine', 'dispatch', 'evasive', 'intercept',
+    'plant', 'classified', 'tracking', 'double', 'gadget', 'stealth'
 ]
